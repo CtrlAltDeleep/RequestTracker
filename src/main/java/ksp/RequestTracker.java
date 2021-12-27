8b993c9950f23af98c52f655c64d34b9d2fe7de5
Avaneesh <avaneesh.deleep@gmail.com>
2021-12-27 15:35:50 +0000

    YESSSSSSS!
    actually builds a working jar w/ mvn

--- src/main/java/ksp/RequestTracker.java
+++ src/main/java/ksp/RequestTracker.java
@@ -1,5 +1,7 @@
 package ksp;
 
+import java.util.Arrays;
+
 public class RequestTracker {
 
   /**
@@ -8,10 +10,11 @@ public class RequestTracker {
    * @param args application command line arguments
    */
   public static void main(String[] args) {
-
+    System.out.println(Arrays.toString(args));
     var app = new TrackerApp();
     app.run(args);
   }
 
+  //mvn clean compile && mvn package && (java -jar target/RequestTracker-1.0-SNAPSHOT-jar-with-dependencies.jar)
 
 }
