99a43a9559e2eed7d034e0a406d85f0b136be471
Avaneesh <avaneesh.deleep@gmail.com>
2022-01-02 14:15:15 +0000

    search now clean-ups punctuation

--- src/main/java/ksp/utilities/SearchUtilities.java
+++ src/main/java/ksp/utilities/SearchUtilities.java
@@ -3,7 +3,8 @@ package ksp.utilities;
 public class SearchUtilities {
 
   public static Integer stringMatchPercentage(String stringToSearch, String searchPhrase){
-    String[] words = searchPhrase.toLowerCase().split(" ");
+    String[] words = searchPhrase.replaceAll("\\p{Punct}", "")
+                                  .toLowerCase().split(" ");
     int matches = 0;
     for (String word:words){
       if (stringToSearch.toLowerCase().contains(word)){
